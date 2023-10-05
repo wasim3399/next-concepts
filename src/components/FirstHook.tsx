e03816b77de426e448e723070506fd35fa5291a3
Wasim Rashid <wasim.rashid@developers.studio>
2023-10-05 12:51:12 +0500

    use-effect

--- src/components/FirstHook.tsx
+++ src/components/FirstHook.tsx
@@ -1,9 +1,14 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function FirstHook() {
 
 const [counter, setCounter] = useState(0);
+const [counter2, setCounter2] = useState(0);
+
+useEffect(() => {
+    console.log('effect called');
+}, [counter]);
 
 const handleIncrement = (num:number) => {
     setCounter(counter + num);
@@ -13,6 +18,14 @@ const handleDecrement = () => {
     setCounter(counter -2)
 }
 
+const handleIncrement2 = (num:number) => {
+    setCounter2(counter2 + num);
+}
+
+const handleDecrement2 = () => {
+    setCounter2(counter2 -2)
+}
+
   return (
     <div>
         <h2>Counter</h2>
@@ -20,6 +33,12 @@ const handleDecrement = () => {
             <button className=' bg-green-400 px-4 py-2 rounded-md' onClick={() => handleIncrement(2)}>+</button>
                 <div className=' px-5'>{counter}</div>
             <button className=' bg-red-400 px-4 py-2 rounded-md' onClick={handleDecrement}>-</button>
+
+            <br />
+
+            <button className=' bg-green-400 px-4 py-2 rounded-md' onClick={() => handleIncrement2(2)}>+</button>
+                <div className=' px-5'>{counter2}</div>
+            <button className=' bg-red-400 px-4 py-2 rounded-md' onClick={handleDecrement2}>-</button>
         </div>
     </div>
   )
